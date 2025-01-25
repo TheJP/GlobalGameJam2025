cfd9e805f41e7fba8675fdf115e3e03332ed932b
TheJP <jp_55555@hotmail.com>
2025-01-25 21:09:26 +0100

    Made boss shoot bubbles

---
+++ enemies/boss/boss_bullet.gd
@@ -0,0 +1,40 @@
+class_name BossBullet
+extends Area2D
+
+
+@export var fly_speed := 50.0
+@export var wavyness := 1.0
+var direction := Vector2.LEFT
+
+
+var _destroyed := false
+var _random_speed := 0.0
+var _spawn_time := 0.0
+
+
+func _ready() -> void:
+	direction = direction.normalized()
+	_random_speed = randf_range(-1.0, 1.0) * (fly_speed * 0.2)
+	_spawn_time = Time.get_ticks_msec()
+
+
+func _process(delta: float) -> void:
+	position += direction * ((fly_speed + _random_speed) * delta)
+	var perpendicular := Vector2(direction.y, -direction.x)
+	position += perpendicular * (sin((Time.get_ticks_msec() - _spawn_time) / 1000.0) * wavyness)
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if not area.is_in_group("fish") and not area.is_in_group("player_bullet"):
+		return
+	if _destroyed:
+		return
+	_destroyed = true;
+
+	var tween := create_tween()
+	tween.tween_property(self, "scale", Vector2.ZERO, 0.05)
+	tween.tween_callback(func() -> void: queue_free())
+
+
+func _on_despawn_timer_timeout() -> void:
+	queue_free()
