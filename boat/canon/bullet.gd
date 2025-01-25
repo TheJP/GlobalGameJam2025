3c212785532ef15922acc62b039687390b1382e0
Flavia Brogle <flavia.brogle@fhnw.ch>
2025-01-25 16:53:00 +0100

    Bullet collides with Dropping Fix bullet added twice

--- boat/canon/bullet.gd
+++ boat/canon/bullet.gd
@@ -1,8 +1,10 @@
 class_name Bullet
-extends Node2D
+extends Area2D
 
 @export var default_speed: float = 1000
-@export var lifetime: float = 2
+@export var lifetime: float = 5
+@export var continue_after_hit: bool = false
+@export var rotation_speed: float = 1
 
 var velocity: Vector2 = Vector2()
 var lifetime_timer: float = 0
@@ -12,10 +14,18 @@ func initialize(shoot_direction: Vector2, spawn_position: Vector2, speed: float 
 	velocity = shoot_direction.normalized() * speed
 	rotation = velocity.angle()
 
+func _ready() -> void:
+	area_entered.connect(on_area_entered)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	position += velocity * _delta
 	lifetime_timer += _delta
+	rotate(rotation_speed * _delta)
 	if lifetime_timer >= lifetime:
 		queue_free()
+
+func on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("enemy"):
+		area.queue_free()
+		if not continue_after_hit:
+			queue_free()
--- level/bullet_spawner.gd
+++ level/bullet_spawner.gd
@@ -9,7 +9,6 @@ func _ready() -> void:
 
 func _on_Boat_shoot(bullet: PackedScene, direction: Vector2, location: Vector2) -> void:
 	var bullet_instance: Bullet = bullet.instantiate()
-	add_child(bullet_instance)
 	bullet_instance.initialize(direction, location)
 	bullet_instance.position = location
 	add_child(bullet_instance)
