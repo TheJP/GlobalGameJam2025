4751594e4788ebae23f13d4e14ae65238c5a7b80
Flavia Brogle <flavia.brogle@fhnw.ch>
2025-01-25 11:06:50 +0100

    Connect Boat to PlayerInput Add BoatBody and BoatBubble

--- boat/boat.gd
+++ boat/boat.gd
@@ -1,5 +1,5 @@
 class_name Boat
-extends Node2D
+extends CharacterBody2D
 
 @export var initial_height:float = 100
 @export var max_height:float = 200
@@ -10,6 +10,7 @@ extends Node2D
 @export var pump_height_add:float = 3
 @export var lose_air_sub:float = 10
 
+@onready var body_node: BoatBody = $Body
 
 func lose_air(amount:float = lose_air_sub):
 	target_height -= amount
@@ -24,16 +25,23 @@ func pump_up(amount:float = pump_height_add):
 
 var target_height:float = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	target_height = initial_height
 
 func _process(_delta):
-	# DEBUG
-	if Input.is_action_just_pressed("ui_up"):
-		pump_up()
-	if Input.is_action_just_pressed("ui_down"):
+	if PlayerInput.is_just_pressed(PlayerInput.Action.DESCEND):
 		lose_air()
+
+	if PlayerInput.is_just_pressed(PlayerInput.Action.ASCEND_A):
+		pump_up()
+	if PlayerInput.is_just_pressed(PlayerInput.Action.ASCEND_B):
+		pump_up()
+
+
+# 	if Input.is_action_just_pressed("ui_up"):
+# 		pump_up()
+# 	if Input.is_action_just_pressed("ui_down"):
+# 		lose_air()
 
 func _physics_process(delta):
 	var speed:float = speed_up if target_height > -position.y else speed_down
