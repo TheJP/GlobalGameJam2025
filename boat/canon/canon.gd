bc36eb5e8b13bb56dff66b031ec2e256b6a8778b
Flavia Brogle <flavia.brogle@fhnw.ch>
2025-01-25 12:57:49 +0100

    Add Canon rotation Bug: doesn't rotate over 0/2pi

--- boat/canon/canon.gd
+++ boat/canon/canon.gd
@@ -1,23 +1,31 @@
 class_name Canon
 extends Node2D
 
-@export var canon_aim_distance:float = 100
-@export var canon_aim_duration:float = 1 # seconds for the canon to move 2pi
+@export var canon_aim_duration:float = 2 # seconds for the canon to move 2pi
+@export var canon_input_deadzone:float = 0.1
 
-@onready var aim_visual_node: Sprite2D = $CanonAimVisual
+@onready var aim_visual_node: Sprite2D = $CanonRotatingVisual
 
 var aim_angle:float = 0
 var aim_angle_target:float = 0
+var is_rotating:bool = false
 
 func _ready() -> void:
-	aim_visual_node.position = Vector2(0, -canon_aim_distance)
+	aim_visual_node.rotation = aim_angle
 
 
 func _process(_delta: float) -> void:
 	var aim_vector: Vector2 = PlayerInput.get_targetting_vector()
-	aim_angle_target = atan2(aim_vector.y, aim_vector.x)
+	if aim_vector.length() > canon_input_deadzone:
+		aim_angle_target = atan2(aim_vector.y, aim_vector.x)
+		is_rotating = true
+	else:
+		is_rotating = false
+
 
 
 func _physics_process(delta: float) -> void:
-	aim_angle = move_toward(aim_angle, aim_angle_target, 2 * PI / canon_aim_duration * delta)
-	aim_visual_node.rotation = aim_angle
+	if is_rotating:
+		# TODO move over 0/2pi
+		aim_angle = move_toward(aim_angle, aim_angle_target, 2 * PI / canon_aim_duration * delta)
+		aim_visual_node.rotation = aim_angle
